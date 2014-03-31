89a730cdb93cfd101f69776c2b26ab260486a873
Raouf Aghrout <aghrout.raouf@gmail.com>
2014-03-31 09:37:48 +0100

    Re-arranged getters to come before setters.

--- Chapter7/src/main/java/com/headfirstjava/chapterseven/boats/Boat.java
+++ Chapter7/src/main/java/com/headfirstjava/chapterseven/boats/Boat.java
@@ -4,12 +4,12 @@ public class Boat {
 
     private int length;
 
-    public void setLength(int len) {
-        length = len;
-    }
-
     public int getLength() {
         return length;
+    }
+
+    public void setLength(int len) {
+        length = len;
     }
 
     public void move() {
